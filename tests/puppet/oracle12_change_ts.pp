434dd282ebb0b528f681155d2ed53320dd0c3971
narmitag <narmitage@vmware.com>
2016-01-04 07:59:36 +0000

    Test Fixes
    remove u12.04
    add NTP to 14.04
    add 2nd dir for oracle to avoid copuing binaries
    
    Signed-off-by: narmitag <narmitage@vmware.com>

--- tests/puppet/oracle12_change_ts.pp
+++ tests/puppet/oracle12_change_ts.pp
@@ -1,5 +1,5 @@
 if $operatingsystem == 'Redhat' and  $::operatingsystemmajrelease == 6 {
 
   class { 'tungsten': installSSHKeys => true, installMysql=> false, mySQLBuild=>'mysql', mySQLVersion=>'5.5',
-        disableFirewall=> false, skipHostConfig=> true ,docker => true , installOracle => true  , oracleVersion=>12, oracleBinaries=>'/mnt/oracle' , oracleTungstenTS=>'puppet_test' }
+        disableFirewall=> false, skipHostConfig=> true ,docker => true , installOracle => true  , oracleVersion=>12, oracleBinaries=>'/mnt2' , oracleTungstenTS=>'puppet_test' }
 }
